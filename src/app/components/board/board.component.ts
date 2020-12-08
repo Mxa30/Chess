a0c941d8509e119ddb1b679a0f3d21db704400d6
Max van der Velde <maxvelde_3010@hotmail.com>
2020-12-08 15:43:03 +0100

    Both black and white pieces can move | King and Pawn regular moves working | TODO: Pawn promotion and Castling

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -130,7 +130,14 @@ export class BoardComponent implements OnInit {
     while (notFoundClickedTile && i < this.board.length) {
       if (this.board[i].tileNum == clickedTile.tileNum) { // Geklikte tile gevonden? Laatste regel is foundClickedTile = true;
         // Check of tile een witte pion heeft : Ja?
-        if (clickedTile.color == 'w') {
+        if (clickedTile.highlighted) { // Check of geklikte tile gehightlightet is : Ja?
+          this.board.forEach(tile => { // Loop door alle tiles heen en vind geselecteerde tile.
+            if (tile.selected) { // Gevonden? : Verplaats geselecteerde tile naar geklikte tile
+              this.movePawn(tile, clickedTile);
+            }
+          });
+        }
+        else if (clickedTile.color != '') {
           // Check of tile al geselecteerd is : Ja?
           if (clickedTile.selected) {
             // Deselecteer tile : ACTIE
@@ -143,6 +150,9 @@ export class BoardComponent implements OnInit {
             // Maak de gebruikte array leeg : ACTIE 
           } else { // Check of tile al geselecteerd is : Nee?  
             // Loop opnieuw door het gehele bord om mogelijke nog geselecteerde tiles te deselecteren : INEFFICIIËNT
+            if (!clickedTile.highlighted){
+
+            }
             this.board.forEach(tile => {
               if (tile.highlighted) { tile.highlighted = false }
               if (tile.selected) { tile.selected = false }
@@ -167,12 +177,6 @@ export class BoardComponent implements OnInit {
               });
             });
           }
-        } else if (clickedTile.highlighted) { // Check of geklikte tile gehightlightet is : Ja?
-          this.board.forEach(tile => { // Loop door alle tiles heen en vind geselecteerde tile.
-            if (tile.selected) { // Gevonden? : Verplaats geselecteerde tile naar geklikte tile
-              this.movePawn(tile, clickedTile);
-            }
-          });
         }
 
         // Check of geklikte tile een lege tile is of een zwarte pion heeft : Ja?
@@ -195,21 +199,65 @@ export class BoardComponent implements OnInit {
   movePawn(from: any, to: any) {
     var fromSplit = from.tileNum.split('');
     var toSplit = to.tileNum.split('');
-    
+
     if (Math.abs(fromSplit[0] - toSplit[0]) > 1 && from.type == 'pawn') { // If pawn moves 2 tiles in one move make en passant possible
-      to.en_passant = true;
+      // to.en_passant = true;
       this.board.forEach((tile, index) => {
         if (tile.tileNum == to.tileNum) {
-          if((this.board[index-1].type == 'pawn' && this.board[index-1].color == 'b') || (this.board[index+1].type == 'pawn' && this.board[index+1].color == 'b')){
-
-            console.log(this.board[index-1].tileNum);
-            console.log(this.board[index+1].tileNum);
-            console.log('en passant possible');
+          // DECLUTTER THIS
+          if (this.board[index - 1].type == 'pawn' && this.board[index - 1].color == 'b' && from.color != 'b') { // If there is a pawn next to destination tile with color black, make en_passant possible 
+            to.en_passant = (parseInt(toSplit[0])-1) + toSplit[1];
+            this.board[index + 8].en_passant = true; // Set tile behind destination tile to en_passant = true;
+            console.log('en passant possible: on white at %s', to.en_passant);
+            console.log(this.board[index + 8].tileNum);
+          }
+          if (this.board[index + 1].type == 'pawn' && this.board[index + 1].color == 'b' && from.color != 'b') { // If there is a pawn next to destination tile with color black, make en_passant possible 
+            to.en_passant = (parseInt(toSplit[0])-1) + toSplit[1];
+            this.board[index + 8].en_passant = true; // Set tile behind destination tile to en_passant = true;
+            console.log('en passant possible: on white at %s', to.en_passant);
+            console.log(this.board[index + 8].tileNum);
+          }
+          if(this.board[index - 1].type == 'pawn' && this.board[index - 1].color == 'w' && from.color != 'w'){ // If there is a pawn next to destination tile with color white, make en_passant possible 
+            to.en_passant = (parseInt(toSplit[0])+1) + toSplit[1];
+            this.board[index - 8].en_passant = true; // Set tile behind destination tile to en_passant = true;
+            console.log('en passant possible: on black at %s', to.en_passant);
+            console.log(this.board[index - 8].tileNum);
+          }
+          if(this.board[index + 1].type == 'pawn' && this.board[index + 1].color == 'w' && from.color != 'w'){ // If there is a pawn next to destination tile with color white, make en_passant possible 
+            to.en_passant = (parseInt(toSplit[0])+1) + toSplit[1];
+            this.board[index - 8].en_passant = true; // Set tile behind destination tile to en_passant = true;
+            console.log('en passant possible: on black at %s', to.en_passant);
+            console.log(this.board[index - 8].tileNum);
           }
         }
       });
-      
     }
+
+    this.board.forEach((tile, index) => {
+      if(tile.en_passant == true && to.tileNum != tile.tileNum && this.board[index + 8].color == 'b'){
+        tile.en_passant = '';
+        this.board[index + 8].en_passant = '';
+      }else if(tile.en_passant == true && to.tileNum != tile.tileNum && this.board[index - 8].color == 'w'){
+        tile.en_passant = '';
+        this.board[index - 8].en_passant = '';
+      }
+    });
+
+    if (to.en_passant) {
+      this.board.forEach(tile => {
+        if (tile.en_passant === to.tileNum) {
+          tile.pawn = '';
+          tile.color = '';
+          tile.type = '';
+          tile.moves = 0;
+          tile.en_passant = '';
+          to.en_passant = '';
+          console.log(to);
+          console.log(tile);
+        }
+      });
+    }
+
     to.pawn = from.pawn;
     to.color = from.color;
     to.type = from.type;
@@ -237,17 +285,17 @@ export class BoardComponent implements OnInit {
       var x = this.columns[this.columns.indexOf(splitArray[1]) + i]
       var coordinate = y + x;
       this.board.forEach(tile => {
-        if (tile.tileNum == coordinate && tile.color != 'w' && coordinate && y > 0 && y < 9) {
+        if (tile.tileNum == coordinate && tile.color != currentPos.color && coordinate && y > 0 && y < 9) {
           allowedTiles.push(coordinate);
         }
       });
     }
     // Mid
     this.board.forEach(tile => {
-      if (tile.tileNum == (y - 1) + this.columns[this.columns.indexOf(splitArray[1]) - 1] && tile.color != 'w') {
+      if (tile.tileNum == (y - 1) + this.columns[this.columns.indexOf(splitArray[1]) - 1] && tile.color != currentPos.color) {
         allowedTiles.push(splitArray[0] + this.columns[this.columns.indexOf(splitArray[1]) - 1]);
       }
-      if (tile.tileNum == (y - 1) + this.columns[this.columns.indexOf(splitArray[1]) + 1] && tile.color != 'w') {
+      if (tile.tileNum == (y - 1) + this.columns[this.columns.indexOf(splitArray[1]) + 1] && tile.color != currentPos.color) {
         allowedTiles.push(splitArray[0] + this.columns[this.columns.indexOf(splitArray[1]) + 1]);
       }
     });
@@ -257,7 +305,7 @@ export class BoardComponent implements OnInit {
       var x = this.columns[this.columns.indexOf(splitArray[1]) + i]
       var coordinate = y + x;
       this.board.forEach(tile => {
-        if (tile.tileNum == coordinate && tile.color != 'w' && coordinate && y > 0 && y < 9) {
+        if (tile.tileNum == coordinate && tile.color != currentPos.color && coordinate && y > 0 && y < 9) {
           allowedTiles.push(coordinate);
         }
       });
@@ -265,27 +313,54 @@ export class BoardComponent implements OnInit {
     return allowedTiles;
   }
 
-  pawnMove(currentPos: any) {
+  pawnMove(currentPos: any) { //TODO: PAWN CAN CHANGE INTO ANY PIECE WHEN IT REACHES ENEMY BORDER
     var splitArray = currentPos.tileNum.split('');
     var allowedTiles: Array<any> = [];
     var y = parseInt(splitArray[0]);
     var x = this.columns[this.columns.indexOf(splitArray[1])]
 
-    var leftDiagonal = y + 1 + this.columns[this.columns.indexOf(splitArray[1]) - 1];
-    var rightDiagonal = y + 1 + this.columns[this.columns.indexOf(splitArray[1]) + 1];
+    if (currentPos.color == 'w') { // IF WHITE MOVES
+      var leftDiagonal = y + 1 + this.columns[this.columns.indexOf(splitArray[1]) - 1];
+      var rightDiagonal = y + 1 + this.columns[this.columns.indexOf(splitArray[1]) + 1];
+    } else { // IF BLACK MOVES
+      var leftDiagonal = y - 1 + this.columns[this.columns.indexOf(splitArray[1]) - 1];
+      var rightDiagonal = y - 1 + this.columns[this.columns.indexOf(splitArray[1]) + 1];
+    }
 
-    this.board.forEach((tile,index) => {
-      if (tile.tileNum == y + 1 + x && tile.color == '') { // Pawn may move 1 tile forward if there isn't any other pawn in front of it
-        allowedTiles.push(tile.tileNum);
+    this.board.forEach((tile, index) => {
+      if (currentPos.color == 'w') { // IF WHITE MOVES
+        if (tile.tileNum == y + 1 + x && tile.color == '') { // Pawn may move 1 tile forward if there isn't any other pawn in front of it
+          allowedTiles.push(tile.tileNum);
+        }
+        if (tile.tileNum == y + 2 + x && currentPos.moves < 1 && tile.color == '' && this.board[index + 8].color == '') { // Pawn may only move 2 tiles foward in their first move
+          allowedTiles.push(tile.tileNum);
+        }
+      } else { // IF BLACK MOVES
+        if (tile.tileNum == y - 1 + x && tile.color == '') { // Pawn may move 1 tile forward if there isn't any other pawn in front of it
+          allowedTiles.push(tile.tileNum);
+        }
+        if (tile.tileNum == y - 2 + x && currentPos.moves < 1 && tile.color == '' && this.board[index - 8].color == '') { // Pawn may only move 2 tiles foward in their first move
+          allowedTiles.push(tile.tileNum);
+        }
       }
-      if (tile.tileNum == y + 2 + x && currentPos.moves < 1 && tile.color != 'w' && this.board[index+8].color == '') { // Pawn may only move 2 tiles foward in their first move
+      // console.log('tile %s',tile.en_passant);
+      console.log(rightDiagonal);
+      if (tile.tileNum == leftDiagonal && (tile.color != currentPos.color && tile.color != '' || tile.en_passant)) { // Pawn may capture other player if it is 1 tile diagional to itself
         allowedTiles.push(tile.tileNum);
+        console.log('diagonal %s',tile.tileNum);
+        console.log(tile.color);
+        console.log(currentPos.color);
+        console.log(tile.en_passant);
       }
-      if (tile.tileNum == leftDiagonal && tile.color == 'b') { // Pawn may capture other player if it is 1 tile diagional to itself
-        allowedTiles.push(tile.tileNum);
+      if(tile.tileNum == rightDiagonal){
+        console.log(tile.color);
+        console.log(currentPos.color);
+        console.log(tile.en_passant); //RAAR
       }
-      if (tile.tileNum == rightDiagonal && tile.color == 'b') { // Pawn may capture other player if it is 1 tile diagional to itself
+      if (tile.tileNum == rightDiagonal && (tile.color != currentPos.color && tile.color != '' || tile.en_passant)) { // Pawn may capture other player if it is 1 tile diagional to itself
         allowedTiles.push(tile.tileNum);
+        console.log('diagonal %s',tile.tileNum);
+        
       }
     });
     return allowedTiles;
